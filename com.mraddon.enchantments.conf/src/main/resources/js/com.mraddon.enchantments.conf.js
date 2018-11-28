9236dd530ecb78f4de31b93ae84157bbc9e20fe1
Raul <raulpelaez@gmail.com>
2018-11-28 16:30:34 +0100

    Ne version

--- com.mraddon.enchantments.conf/src/main/resources/js/com.mraddon.enchantments.conf.js
+++ com.mraddon.enchantments.conf/src/main/resources/js/com.mraddon.enchantments.conf.js
@@ -2,5 +2,5 @@ jQuery(document).ready(function() {
     //For external links only:
     jQuery(".external-link").attr("target", "_blank");
     //For all type of links (internal and external):
-    jQuery(".wiki-content a").attr("target", "_blank");
+    //jQuery(".wiki-content a").attr("target", "_blank");
 });
